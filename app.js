e226de9d7493572ae736060e349b8d5b9abbbaaf
Jona-Medina <jonaa.medina99@gmail.com>
2021-01-30 23:04:29 -0300

    MercadoLiebre Final

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-port = process.env.PORT || '3000';
-
-app.listen(port, ()=>{
+app.listen( process.env.PORT || 3000, ()=>{
      console.log('Servidor corriendo');
 });
 
